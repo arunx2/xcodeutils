dee09edcb3debc95b489d8540be9e74e2f2356a8
Ahmed Hamdy <dimo.hamdy@gmail.com>
2014-07-18 23:53:28 +0200

    support StoryBoard and other image Type
    support StoryBoard and other image Type

--- find-unused-resources.sh
+++ find-unused-resources.sh
@@ -10,17 +10,17 @@ unusedfiles="";
 let count=0;
 let totalsize=0;
 # collect the files needs to be introspected
-project=`find $1 -name '*.?ib' -o -name '*.[mh]'`
+project=`find $1 -name '*.?ib' -o -name '*.[mh]' -o -name '*.storyboard'`
 
-for i in `find $1 -name "*.png" -o -name "*.jpg"`; do 
-    file=`basename -s .jpg "$i" | xargs basename -s .png | xargs basename -s @2x`
-    if ! grep -q $file $project; then
-        filesize=`stat -f "%z" $i`;
-        filesizekb=`echo "$filesize 1024.0" | awk '{printf "%.2f", $1 / $2}'`
-        unusedfiles="$unusedfiles <br> <a href=\"$i\">$i</a> ($filesizekb kb)";
-        let "count += 1";
-        let "totalsize += $filesize"
-    fi
+for i in `find $1 -name '*.gif' -o -name '*.jpg' -o -name '*.png' -o -name '*.jpeg'`; do
+file=`basename -s .jpg "$i" | xargs basename -s .png | xargs basename -s @2x`
+if ! grep -q $file $project; then
+filesize=`stat -f "%z" $i`;
+filesizekb=`echo "$filesize 1024.0" | awk '{printf "%.2f", $1 / $2}'`
+unusedfiles="$unusedfiles <br> <a href=\"$i\">$i</a> ($filesizekb kb)";
+let "count += 1";
+let "totalsize += $filesize"
+fi
 done
 #construct body
 totalsizekb=`echo "$totalsize 1024.0" | awk '{printf "%.2f", $1 / $2}'`
@@ -31,7 +31,7 @@ echo "</h3>"
 echo "<pre>"
 #generate body content if there are unused files.
 if [ $count > 0 ]; then
-    echo $unusedfiles;
+echo $unusedfiles;
 fi
 echo "</pre>"
 #---------------------------------------------------------------------------------------
@@ -41,12 +41,12 @@ count=0;
 unusedfiles="";
 project=`find $1 -name '*.pbxproj'`
 
-for i in `find $1 -name "*.[hmca]" -o -name "*.cpp"`; do 
-    file=`basename "$i"`
-    if ! grep -q $file $project; then
-        unusedfiles="$unusedfiles <br> $i";
-        let "count = count + 1";
-    fi
+for i in `find $1 -name "*.[hmca]" -o -name "*.cpp"`; do
+file=`basename "$i"`
+if ! grep -q $file $project; then
+unusedfiles="$unusedfiles <br> $i";
+let "count = count + 1";
+fi
 done
 echo "<i> <b>Note:</b> This scans all source files (*.h, *.m, *.c, *.a, *.cpp) references in all pbxproj definitions. Once it is added into project definitions, it is considered being used.</i> <br>"
 
@@ -56,7 +56,7 @@ echo "</h3>"
 echo "<pre>"
 #generate body content if there are unused files.
 if [ $count > 0 ]; then
-    echo $unusedfiles;
+echo $unusedfiles;
 fi
 echo "</pre>"
 echo "</body>"
